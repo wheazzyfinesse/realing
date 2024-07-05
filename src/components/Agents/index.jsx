798ac5753d5a88700a3b1f46ee1e117265107235
wheazzyfinesse <59867253+wheazzyfinesse@users.noreply.github.com>
2024-07-06 01:02:33 +0200

    second commit about, properties, agent section done

--- src/components/Agents/index.jsx
+++ src/components/Agents/index.jsx
@@ -1,7 +1,82 @@
+import Slider from "react-slick";
 import "./Agents.css";
+import { agents } from "../../sources";
+import React, { useRef } from "react";
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
 
 const Agents = () => {
-	return <div>Agents</div>;
+	const ref = useRef(null);
+	const settings = {
+		Infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		dots: true,
+		centerMode: true,
+		gap: 50,
+		pauseOnHover: true,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		centerPadding: 0,
+		responsive: [
+			{
+				breakpoint: 900,
+				settings: {
+					slidesToShow: 2,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+				},
+			},
+		],
+	};
+	return (
+		<section id="agents">
+			<div className="wrapper">
+				<h1 className="heading-1">
+					<span className="gradient-text">Agents</span>
+				</h1>
+				<Slider {...settings} className="agent-container" ref={ref}>
+					{agents.map((agent, index) => (
+						<React.Fragment key={index}>
+							<div className="profile">
+								<img src={agent.profile} alt={agent.name} />
+							</div>
+							<h3 className="name">{agent.name}</h3>
+							<div className="flex-center social-container">
+								{agent.socialHandles.map((list, i) => (
+									<a
+										href={list.url}
+										key={i}
+										target="_blank"
+										className="flex-center icon-wrapper"
+									>
+										{list.icon}
+									</a>
+								))}
+							</div>
+						</React.Fragment>
+					))}
+				</Slider>
+				<div className="flex-center buttons-container">
+					<button
+						className="flex-center btn"
+						onClick={() => ref.current.slickPrev()}
+					>
+						<FaLongArrowAltLeft />
+					</button>
+					<button
+						className="flex-center btn"
+						onClick={() => ref.current.slickNext()}
+					>
+						<FaLongArrowAltRight />
+					</button>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Agents;
--- src/components/Properties/index.jsx
+++ src/components/Properties/index.jsx
@@ -1,7 +1,46 @@
 import "./Properties.css";
+import { properties } from "../../sources";
+import { FaBath, FaBed } from "react-icons/fa6";
 
 const Properties = () => {
-	return <div>Properties</div>;
+	return (
+		<section id="properties">
+			<div className="wrapper">
+				<h1 className="heading-1">
+					<span className="gradient-text">Featured Properties</span>
+				</h1>
+				<div className="properties-container">
+					{properties.map((property, index) => (
+						<div className="property" key={index}>
+							<div className="top picture">
+								<img src={property.image} alt={property.title} />
+							</div>
+							<div className="middle">
+								<div className="row flex">
+									<h3 className="title clamp-2">{property.title}</h3>
+									<h1 className="price">{property.price}</h1>
+								</div>
+								<p className="muted description">{property.description}</p>
+							</div>
+							<div className="bottom">
+								<div className="row flex">
+									<div className="flex-center btn">
+										<FaBed />
+										{property.bedrooms}
+									</div>
+									<div className="flex-center btn">
+										<FaBath />
+										{property.bathrooms}
+									</div>
+								</div>
+								<div className="btn primary">View Property</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Properties;
